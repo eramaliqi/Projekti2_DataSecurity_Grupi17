e37d093de68e3cfbc6e7dbbb12f2cea6387961a1
Erand12334 <erandkurtaliqi1@gmail.com>
2023-04-13 22:15:52 +0200

    test

--- TEST.java
+++ TEST.java
@@ -1,2 +1,14 @@
 public class TEST {
+
+    private String encrypt(String input, int shift) {
+        StringBuilder result = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char base = Character.isLowerCase(c) ? 'a' : 'A';
+                c = (char) (base + (c - base + shift) % 26);
+            }
+            result.append(c);
+        }
+        return result.toString();
+    }
 }
